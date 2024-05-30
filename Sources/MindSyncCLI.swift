78ff30ccf4b681a89926e1bdb01316c9d00a3cc9
Andreas <andreasneacsu@gmail.com>
2024-05-30 15:45:24 -0700

    fixed list command

--- Sources/MindSyncCLI.swift
+++ Sources/MindSyncCLI.swift
@@ -7,7 +7,10 @@ struct MindSyncCLI: ParsableCommand {
     abstract: "A Swift todo command-line tool",
     subcommands: [new.self, edit.self, delete.self, list.self]
   )
-
+  mutating func run() throws{
+    let api = API()
+    api.makeRequest(endpoint: "/users/2/habit_trackers/")
+  }
 }
 
 struct new: ParsableCommand {
@@ -22,16 +25,29 @@ struct new: ParsableCommand {
 }
 
 struct edit: ParsableCommand {
-  @Argument var taskID: String
+  @Argument(help: "The ID of the task to edit")
+  var taskID: String
   @Option(help: "Edit the task title") 
-  var newTitle: String
+  var newTitle: String?
   @Option(help: "Edit the task completion")
-  var newStatus: Bool
+  var newStatus: Bool?
   static let configuration = CommandConfiguration(abstract: "Edits a task")
   mutating func run() throws{
     let app = TodoApp()
-    if let editedTodo = app.editTodo(id: self.taskID, title: newTitle, completed: newStatus) {
-        print("Edited todo: \(editedTodo.title), Completed: \(editedTodo.completed)")
+    var editedTodo: TodoItem?
+
+    if let newTitle = newTitle, !newTitle.isEmpty {
+      editedTodo = app.editTodo(id: self.taskID, title: newTitle, completed: newStatus)
+      print("Edited todo: \(editedTodo?.title ?? "No changes made"), Completed: \(editedTodo?.completed ?? false)")
+    } else if let newStatus = newStatus {
+      editedTodo = app.updateTodoStatus(id: self.taskID, completed: newStatus)
+      print("Updated todo status: \(editedTodo?.title ?? "No changes made"), Completed: \(editedTodo?.completed ?? false)")
+    }
+
+    if let editedTodo = editedTodo {
+      print("Edited todo: \(editedTodo.title), Completed: \(editedTodo.completed)")
+    } else {
+      print("No changes made.")
     }
   }
 }
